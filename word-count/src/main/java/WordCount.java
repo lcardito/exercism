1606d1195287750831c51007de600db15e4a63de
Luigi Cardito <lcardito@gmail.com>
2016-05-30 19:33:44 +0100

    LC: word count v1.2

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -1,12 +1,12 @@
 import java.util.Map;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class WordCount {
 
-    public Map<String, Integer> phrase(String word) {
-        return Stream.of(word.replaceAll("[^a-zA-Z1-9 ]", "").toLowerCase().split("\\s+"))
+    public Map<String, Integer> phrase(String phrase) {
+        return Pattern.compile("\\W+").splitAsStream(phrase.toLowerCase())
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(w -> 1)));
     }
 }
