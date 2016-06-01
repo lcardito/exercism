bf3f0a74cdce82cf7379e3cf83d73dc98c93cbce
Luigi Cardito <lcardito@gmail.com>
2016-06-01 17:43:30 +0100

    LC: make it work first

--- anagram/src/main/java/Anagram.java
+++ anagram/src/main/java/Anagram.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.lang.Math.toIntExact;
 
 public class Anagram {
 
@@ -13,17 +17,31 @@ public class Anagram {
     public List<String> match(List<String> strings) {
         ArrayList<String> result = new ArrayList<>();
         for (String current : strings) {
-            if (current.length() != diaper.length()) {
+            String diaperLowerCase = diaper.toLowerCase();
+            String currentLowerCase = current.toLowerCase();
+
+            if (diaperLowerCase.equals(currentLowerCase)) {
                 continue;
             }
-            List<Character> wantedChars = diaper.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
-            List<Character> actualChars = current.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
 
-            if (actualChars.containsAll(wantedChars)) {
+            Map<Character, Integer> diaperOccurrences = getCharsOccurrences(diaperLowerCase);
+            Map<Character, Integer> currentOccurrences = getCharsOccurrences(currentLowerCase);
+
+            if (diaperOccurrences.equals(currentOccurrences)) {
                 result.add(current);
             }
 
         }
         return result;
+    }
+
+    private Map<Character, Integer> getCharsOccurrences(String diaper) {
+        return diaper.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        c -> toIntExact(diaper.chars().mapToObj(i -> (char) i).filter(c2 -> c2 == c).count()),
+                        (char1, char2) -> char1
+                ));
     }
 }
