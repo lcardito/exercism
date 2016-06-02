96cac9978b7dd702311a993bde1c148360f98c72
Luigi Cardito <lcardito@gmail.com>
2016-06-02 13:10:59 +0100

    LC: now I like it

--- anagram/src/main/java/Anagram.java
+++ anagram/src/main/java/Anagram.java
@@ -1,10 +1,5 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import static java.lang.Math.toIntExact;
 
 public class Anagram {
 
@@ -15,33 +10,17 @@ public class Anagram {
     }
 
     public List<String> match(List<String> strings) {
-        ArrayList<String> result = new ArrayList<>();
-        for (String current : strings) {
-            String diaperLowerCase = diaper.toLowerCase();
-            String currentLowerCase = current.toLowerCase();
-
-            if (diaperLowerCase.equals(currentLowerCase)) {
-                continue;
-            }
-
-            Map<Character, Integer> diaperOccurrences = getCharsOccurrences(diaperLowerCase);
-            Map<Character, Integer> currentOccurrences = getCharsOccurrences(currentLowerCase);
-
-            if (diaperOccurrences.equals(currentOccurrences)) {
-                result.add(current);
-            }
-
-        }
-        return result;
+        return strings.stream()
+                .filter(s -> !s.equalsIgnoreCase(diaper))
+                .filter(s -> s.length() == diaper.length())
+                .filter(s -> sort(diaper).equals(sort(s)))
+                .collect(Collectors.toList());
     }
 
-    private Map<Character, Integer> getCharsOccurrences(String diaper) {
-        return diaper.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.toMap(
-                        Function.identity(),
-                        c -> toIntExact(diaper.chars().mapToObj(i -> (char) i).filter(c2 -> c2 == c).count()),
-                        (char1, char2) -> char1
-                ));
+    private String sort(String in) {
+        return in.toLowerCase()
+                .chars()
+                .mapToObj(c -> String.valueOf((char) c))
+                .sorted().collect(Collectors.joining());
     }
 }
