f4b6d45ae8f089d647662b6b4aef869bf01373cc
Luigi Cardito <lcardito@gmail.com>
2016-06-07 17:14:41 +0100

    LC: is it getting better at all?

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,2 +1,13 @@
+import java.util.stream.IntStream;
+
+import static java.lang.Math.toIntExact;
+
 public class Hamming {
+    public static Integer compute(String firstSequence, String secondSequence) {
+        if (firstSequence.length() != secondSequence.length()) {
+            throw new IllegalArgumentException();
+        }
+        return toIntExact(IntStream.range(0, firstSequence.length())
+                .filter(i -> firstSequence.charAt(i) != secondSequence.charAt(i)).count());
+    }
 }
