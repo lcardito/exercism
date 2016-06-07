67991e8bb9c20495af03feb618ab639f2abc6c31
Luigi Cardito <lcardito@gmail.com>
2016-06-07 17:22:28 +0100

    LC: print me the solution

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -7,7 +7,18 @@ public class Hamming {
         if (firstSequence.length() != secondSequence.length()) {
             throw new IllegalArgumentException();
         }
+        System.out.println(firstSequence);
+        System.out.println(secondSequence);
         return toIntExact(IntStream.range(0, firstSequence.length())
-                .filter(i -> firstSequence.charAt(i) != secondSequence.charAt(i)).count());
+                .filter(i -> {
+                    boolean sameChar = firstSequence.charAt(i) != secondSequence.charAt(i);
+                    if (sameChar){
+                        System.out.print("^");
+                    } else {
+                        System.out.print(" ");
+                    }
+
+                    return sameChar;})
+                .count());
     }
 }
