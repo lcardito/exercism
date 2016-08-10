b64c0d85bd112b66444e9e5a8ac7b9ac4a41d16a
Luigi Cardito <lcardito@gmail.com>
2016-08-10 15:17:01 +0100

    This was easy

---
+++ hackerrank-algo/src/main/java/me/lcardito/hackerrank/code/eight/Solution.java
@@ -0,0 +1,30 @@
+package me.lcardito.hackerrank.code.eight;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class Solution {
+
+    public static void main(String[] argh) {
+        Map<String, Integer> phoneBook = new HashMap<>();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name, phone);
+
+        }
+        while (in.hasNext()) {
+            String s = in.next();
+            if (phoneBook.containsKey(s)) {
+                System.out.printf("%s=%d\n", s, phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
