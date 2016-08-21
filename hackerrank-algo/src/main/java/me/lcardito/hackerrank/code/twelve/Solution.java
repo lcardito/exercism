cdec394b5e326bbc5f657120fcfb4692347c21ce
Luigi Cardito <lcardito@gmail.com>
2016-08-21 14:47:22 +0100

    wip

--- hackerrank-algo/src/main/java/me/lcardito/hackerrank/code/twelve/Solution.java
+++ hackerrank-algo/src/main/java/me/lcardito/hackerrank/code/twelve/Solution.java
@@ -3,7 +3,8 @@ package me.lcardito.hackerrank.code.twelve;
 import java.util.Scanner;
 
 public class Solution {
-    static void main(String[] args) {
+
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String firstName = scan.next();
         String lastName = scan.next();
--- hackerrank-algo/src/main/java/me/lcardito/hackerrank/code/twelve/Student.java
+++ hackerrank-algo/src/main/java/me/lcardito/hackerrank/code/twelve/Student.java
@@ -1,9 +1,31 @@
 package me.lcardito.hackerrank.code.twelve;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.DoublePredicate;
+import java.util.function.Function;
 
 public class Student extends Person {
     private int[] testScores;
+    private static final Map<DoublePredicate, String> GRADE_RULES = new HashMap<>();
+
+    static {
+        DoublePredicate isOGrade = a -> a >= 90 && a <= 100;
+        DoublePredicate isEGrade = a -> a >= 80 && a < 90;
+        DoublePredicate isAGrade = a -> a >= 70 && a < 80;
+        DoublePredicate isPGrade = a -> a >= 55 && a <= 70;
+        DoublePredicate isDGrade = a -> a >= 40 && a <= 55;
+        DoublePredicate isTGrade = a -> a < 40;
+
+        GRADE_RULES.put(isOGrade, "O");
+        GRADE_RULES.put(isEGrade, "E");
+        GRADE_RULES.put(isAGrade, "A");
+        GRADE_RULES.put(isPGrade, "P");
+        GRADE_RULES.put(isDGrade, "D");
+        GRADE_RULES.put(isTGrade, "T");
+    }
+
 
     public Student(String firstName, String lastName, int id, int[] testScores) {
         super(firstName, lastName, id);
@@ -11,47 +33,12 @@ public class Student extends Person {
     }
 
     public String calculate() {
-        String grade = "";
         double avg = Arrays.stream(testScores).average().orElse(0);
 
-        if (isOGrade(avg)) {
-            grade = "O";
-        } else if (isEGrade(avg)) {
-            grade = "E";
-        } else if (isAGrade(avg)) {
-            grade = "A";
-        } else if (isPGrade(avg)) {
-            grade = "P";
-        } else if (isDGrade(avg)) {
-            grade = "D";
-        } else if (isTGrade(avg)) {
-            grade = "T";
-        }
+        DoublePredicate result = GRADE_RULES.keySet().stream().filter(p -> p.test(avg)).findFirst().orElse(null);
 
-        return grade;
+        return GRADE_RULES.getOrDefault(result, "T");
     }
 
-    private boolean isTGrade(double avg) {
-        return avg < 40;
-    }
-
-    private boolean isDGrade(double avg) {
-        return avg >= 40 && avg <= 55;
-    }
-
-    private boolean isPGrade(double avg) {
-        return avg >= 55 && avg <= 70;
-    }
-
-    private boolean isAGrade(double avg) {
-        return avg >= 70 && avg < 80;
-    }
-
-    private boolean isEGrade(double avg) {
-        return avg >= 80 && avg < 90;
-    }
-
-    private boolean isOGrade(double a) {
-        return a >= 90 && a <= 100;
-    }
+    private final Function getGrade = (Function) o -> null;
 }
