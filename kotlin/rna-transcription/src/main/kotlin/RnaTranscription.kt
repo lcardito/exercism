d8febf636e781b9d0a340bc497baa8b7472fc100
luigicardito <lcardito@gmail.com>
2018-02-13 08:56:51 +0000

    when format a lot better.

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,2 +1,10 @@
-val mapping = hashMapOf("G" to "C", "C" to "G", "T" to "A", "A" to "U")
-fun transcribeToRna(dna: String): String = dna.map { it -> mapping[it.toString()] }.joinToString(separator = "")
+fun transcribeToRna(dna: String): String =
+        dna.fold("", { acc, ch ->
+            when (ch) {
+                'G' -> acc + "C"
+                'C' -> acc + "G"
+                'T' -> acc + "A"
+                'A' -> acc + "U"
+                else -> throw Error("Char not found for transcription")
+            }
+        })
